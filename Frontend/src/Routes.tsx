9dca39bfe5bcaeab4f1d97a17f63fe61dbf26dcf
CyrusClasher <cyrusclasher08@gmail.com>
2025-01-07 19:11:19 +0530

    made some changes

--- Frontend/src/Routes.tsx
+++ Frontend/src/Routes.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 // import {Home,NotFound} from "pages/home";
 import { useRoutes } from "react-router-dom";
 import ServerInfo from "./pages/serverinfo/index";
